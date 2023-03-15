a961b4c051f3c503213f281f9d35fb0776858390
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-03-15 20:55:44 +0000

    Add alternating turns
    Add a second player where each player picks a cell one by one.

--- src/MakeMove.php
+++ src/MakeMove.php
@@ -1,34 +1,35 @@
 <?php
-function makeMove(&$board)
+function makeMove(&$board, $symbol)
 {
-  $move = readline("\nPlease enter the number of the cell you would like to take:\n");
+  $playerNumber = $symbol == "X" ? 1 : 2;
+  $move = readline("\nPlayer $playerNumber, please pick a cell from 1 to 9:\n");
   switch ($move) {
     case "1":
-      $board[0][0] = "X";
+      $board[0][0] = $symbol;
       break;
     case "2":
-      $board[0][1] = "X";
+      $board[0][1] = $symbol;
       break;
     case "3":
-      $board[0][2] = "X";
+      $board[0][2] = $symbol;
       break;
     case "4":
-      $board[1][0] = "X";
+      $board[1][0] = $symbol;
       break;
     case "5":
-      $board[1][1] = "X";
+      $board[1][1] = $symbol;
       break;
     case "6":
-      $board[1][2] = "X";
+      $board[1][2] = $symbol;
       break;
     case "7":
-      $board[2][0] = "X";
+      $board[2][0] = $symbol;
       break;
     case "8":
-      $board[2][1] = "X";
+      $board[2][1] = $symbol;
       break;
     case "9":
-      $board[2][2] = "X";
+      $board[2][2] = $symbol;
       break;
   }
 }
--- src/TicTacToe.php
+++ src/TicTacToe.php
@@ -4,8 +4,10 @@ include "./MakeMove.php";
 
 echo "Welcome to Tic-Tac-Toe!\n";
 $board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
+$turn = 1;
 
 while (true) {
   displayBoard($board);
-  makeMove($board);
+  $turn == 1 ? makeMove($board, "X") : makeMove($board, "O");
+  $turn *= -1;
 }
