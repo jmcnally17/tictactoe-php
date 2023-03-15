2c287ec08ed54a71ef2a436204c57c69bf864d27
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-03-15 21:21:12 +0000

    Move winner declaration into conditional

--- src/TicTacToe.php
+++ src/TicTacToe.php
@@ -10,8 +10,9 @@ $turn = 1;
 while (true) {
   displayBoard($board);
   $turn == 1 ? makeMove($board, "X") : makeMove($board, "O");
-  if (hasAPlayerWon($board)) break;
+  if (hasAPlayerWon($board)) {
+    echo "\nCongratulations player " . ($turn == 1 ? 1 : 2) . ", you win!\n";
+    break;
+  }
   $turn *= -1;
 }
-
-echo "\nCongratulations player " . ($turn == 1 ? 1 : 2) . ", you win!\n";
